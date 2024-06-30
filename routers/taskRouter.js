752c92674631125a258c53a88152f0c219bbbcb9
Anushka <as0846403@gmail.com>
2024-06-30 18:17:04 +0530

    delete task api updated

--- routers/taskRouter.js
+++ routers/taskRouter.js
@@ -5,6 +5,7 @@ const updateCategory = require("../apis/updateCategory.js");
 const updateCheckMark = require("../apis/updateCheckMark.js");
 const getTaskById = require("../apis/getTaskById.js");
 const updateTask = require("../apis/updateTask.js");
+const deleteTask = require("../apis/deleteTask.js");
 const router = express.Router();
 
 router.post("/createTask",verifyToken,createTask);
@@ -12,5 +13,5 @@ router.patch("/updateCategory/:id",updateCategory);
 router.patch("/updateCheckMark/:id",updateCheckMark);
 router.get("/getTaskById/:id",getTaskById);
 router.patch("/updateTask/:id",verifyToken,updateTask);
-
+router.delete("/deleteTask/:id",verifyToken,deleteTask);
 module.exports = router;
